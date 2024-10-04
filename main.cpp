133ab8f04027360b62b444832b1dd0962e833ce1
Dylan Smith <ddsmith9254@gmail.com>
2024-10-03 23:23:39 -0700

    made more resistant to input error

--- main.cpp
+++ main.cpp
@@ -4,12 +4,17 @@ int main(void) {
     //TODO eliminate soft locks throughout main
     //gather preliminary information needed to begin sequence
     std::println("MODE: \n(1)Manual\n(2)Random");
-    int selection = 0;
-    while (selection > 2 || selection < 1) std::cin >> selection;
+    int selection;
+    while (!(std::cin >> selection) || selection > 2 || selection < 1) {
+        std::print("\033[A\33[2K\r");
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
     
     std::print("Count: ");
     unsigned long long count;
     while (!(std::cin >> count)) {
+        std::print("\033[A\33[2K\rCount: ");
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
@@ -20,20 +25,27 @@ int main(void) {
         break;
     case 2: //random surveying
         //gather additional necessary information
-        unsigned long long maxN;
         std::print("Upper Bound (0 for max): ");
+        unsigned long long maxN;
         while (!(std::cin >> maxN)) {
+            std::print("\033[A\33[2K\rUpper Bound (0 for max): ");
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
+
         if (maxN == 0) maxN = std::numeric_limits<unsigned long long>::max();
 
-        char answer;
         std::print("Show individual results? (Y/n) ");
+        char answer;
         do {
-            std::cin >> answer;
+            while (!(std::cin >> answer)) {
+                std::print("\033[A\33[2K\rShow individual results? (Y/n) ");
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
             answer = tolower(answer);
         } while (answer != 'y' && answer != 'n');
+
         std::println("\n");
 
         sequence::testRandomNumberFactorizations(count, answer == 'y', maxN);
