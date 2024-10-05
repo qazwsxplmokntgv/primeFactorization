bfd7c6d4dc5d095fbbdeb16f1161a5ce8597d8cc
Dylan Smith <ddsmith9254@gmail.com>
2024-10-05 16:18:23 -0700

    corrected mistaken use of const&& (&& intended)

--- utils.hpp
+++ utils.hpp
@@ -5,5 +5,5 @@
 constexpr size_t panelWidth = 64;
 constexpr size_t miniPanelWidth = panelWidth / 2;
 
-void printDivider(const std::string&& leftHeader = "", const std::string&& rightHeader = "");
+void printDivider(std::string&& leftHeader = "", std::string&& rightHeader = "");
 
