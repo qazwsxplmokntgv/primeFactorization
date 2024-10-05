54d74c899a3832c1c017b31a7476c7e150090d3a
Dylan Smith <ddsmith9254@gmail.com>
2024-10-05 16:10:07 -0700

    small changes to line spacing, added a screen clear to manual mode

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ int main(void) {
 
     switch (selection) {
     case 1: //manual entry
+        std::print("\033[2J\033[H");
         sequence::findUserRequestedFactorizations(count);
         break;
     case 2: //random surveying
@@ -46,7 +47,7 @@ int main(void) {
             answer = tolower(answer);
         } while (answer != 'y' && answer != 'n');
 
-        std::println("\n");
+        std::println("{}", answer == 'n' ? "\n" : "");
 
         sequence::testRandomNumberFactorizations(count, answer == 'y', maxN);
         break;
