9c6506a842480bc4cd04869ef4476bc9cb452211
Dylan Smith <ddsmith9254@gmail.com>
2024-10-05 20:19:52 -0700

    moved printDivider to be a nonmember function in statset.cpp, eliminating the need for utils.cpp

--- utils.cpp
+++
@@ -1,6 +0,0 @@
-#include "utils.hpp"
-
-void printDivider(std::string&& leftHeader, std::string&& rightHeader) {
-    static constexpr size_t indent = 3;
-    std::println("\033[1;4;97;53m{:-<{}}{:-<{}}{:-<{}}\033[0m", "", indent, leftHeader, panelWidth, rightHeader, panelWidth - indent);
-}
--- utils.hpp
+++ utils.hpp
@@ -1,9 +1,4 @@
 #pragma once
 
-#include <print>
-
 constexpr size_t panelWidth = 64;
 constexpr size_t miniPanelWidth = panelWidth / 2;
-
-void printDivider(std::string&& leftHeader = "", std::string&& rightHeader = "");
-
