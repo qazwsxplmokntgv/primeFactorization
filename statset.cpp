e5481565389a132b50981d7e1180b4d0ee6b80fc
Dylan Smith <ddsmith9254@gmail.com>
2024-10-21 13:41:18 -0700

    renames to better communicate function purposes

--- statset.cpp
+++ statset.cpp
@@ -1,10 +1,11 @@
 #include "statset.hpp"
 
-StatSet::StatSet(const uint64_t inputCount_) : inputCount(inputCount_), 
-    fastest          (std::min(inputCount, (uint64_t)std::max((unsigned int)log10l(inputCount), 3u))), 
-    slowest          (std::min(inputCount, (uint64_t)std::max((unsigned int)log10l(inputCount), 3u))), 
-    mostFactors      (std::min(inputCount, (uint64_t)std::max((unsigned int)log10l(inputCount), 3u))), 
-    mostUniqueFactors(std::min(inputCount, (uint64_t)std::max((unsigned int)log10l(inputCount), 3u))) {
+StatSet::StatSet(const size_t inputCount_) : inputCount(inputCount_), 
+    //size of ranking lists should never be less than 3 unless there are fewer than 3 inputs, and should scale as inputCount grows (specifically in accordance to log10 works well)
+    fastest          (std::min(inputCount, static_cast<size_t>(std::max(log10(inputCount), 3.)))), 
+    slowest          (std::min(inputCount, static_cast<size_t>(std::max(log10(inputCount), 3.)))), 
+    mostFactors      (std::min(inputCount, static_cast<size_t>(std::max(log10(inputCount), 3.)))), 
+    mostUniqueFactors(std::min(inputCount, static_cast<size_t>(std::max(log10(inputCount), 3.)))) {
     timesData.reserve(inputCount);
 }
 
@@ -15,11 +16,11 @@ void StatSet::printout(void) const {
     printDivider("Fastest Factorizations Attempted", "Slowest Factorizations Attempted");
     RankingList::printRecordLists(fastest, slowest);
     
-    //as above, for factorizations with the most factors
+    //same as above, for factorizations with the most factors
     printDivider("Factorizations With Most Total Factors", "Factorizations With Most Unique Factors");
     RankingList::printRecordLists(mostFactors, mostUniqueFactors, 
-        [](size_t i, const RankingList& leftList){ return std::format("#{}: {} | {}", i + 1, leftList.viewEntryAt(i).factorization.getFactorCount(), leftList.viewEntryAt(i).calcTime); },
-        [](size_t i, const RankingList& rightList){ return std::format("#{}: {} | {}", i + 1, rightList.viewEntryAt(i).factorization.getUniqueFactorCount(), rightList.viewEntryAt(i).calcTime); });
+        [](size_t i, const RankingList& leftList ){ return std::format("#{}: {} | {}", i + 1, std::next(leftList.cbegin(),  i)->factorization.getFactorCount(), std::next(leftList.cbegin(), i)->calcTime); },
+        [](size_t i, const RankingList& rightList){ return std::format("#{}: {} | {}", i + 1, std::next(rightList.cbegin(), i)->factorization.getUniqueFactorCount(), std::next(rightList.cbegin(), i)->calcTime); });
 
     //various statistical facts regarding calculation times 
     printDivider("Calculation Times");
@@ -39,66 +40,62 @@ void StatSet::printout(void) const {
         std::format("{}{}", "Q4: ", inputCount ? timesData.back() : std::chrono::duration<long double, std::milli>(0)), miniPanelWidth, 
         std::format("{}{}", "Standard Deviation: ", stdDev));
     
-    //prints an overview of the calculation time distribution
+    //overview of the calculation time distribution
     printDivider("Counts (fastest applicable category only)");
-    categories.printout();
+    timeCategories.printout();
 }
 
 void StatSet::handleNewTime(FactorCalculationInfo&& newFactorization) {
-    //compares new item against each ranking list, inserting if and when appropriate
-    fastest.checkAndRank(newFactorization);
-    slowest.checkAndRank(newFactorization);
-    mostFactors.checkAndRank(newFactorization);
-    mostUniqueFactors.checkAndRank(newFactorization);
+    fastest.rankIfApplicable(newFactorization);
+    slowest.rankIfApplicable(newFactorization);
+    mostFactors.rankIfApplicable(newFactorization);
+    mostUniqueFactors.rankIfApplicable(newFactorization);
   
-    //records the calculation time for calculation of a few statistical measures after completion
-    timesData.emplace_back(newFactorization.calcTime);
+    timesData.push_back(newFactorization.calcTime);
 }
 
 void StatSet::completeFinalCalculations(void) {
-    //avoid division by 0
+    //avoid division by 0 - leaves duration values at default constructed 0
     if (!inputCount) return;
 
-    //places the collected times in order
-    std::sort(timesData.begin(), timesData.end());
+    std::ranges::sort(timesData, std::less());
 
-    //calculates the quartiles
-    firstQuart = getWeightedAverage<std::chrono::duration<long double, std::milli>>((timesData.size() - 1) * .25, timesData);
-    median =     getWeightedAverage<std::chrono::duration<long double, std::milli>>((timesData.size() - 1) * .5,  timesData);
-    thirdQuart = getWeightedAverage<std::chrono::duration<long double, std::milli>>((timesData.size() - 1) * .75, timesData);
+    firstQuart = interpolateDataAtPercentile<std::chrono::duration<long double, std::milli>>(timesData, .25);
+    median =     interpolateDataAtPercentile<std::chrono::duration<long double, std::milli>>(timesData, .5);
+    thirdQuart = interpolateDataAtPercentile<std::chrono::duration<long double, std::milli>>(timesData, .75);
 
-    //calculates the weight of the values at the ends of the iqr and adds them to the interquartile sum
+    //because the edge elements of the inner quartiles may have unique weights compared to the other elements, they are handled separately
     std::chrono::duration<long double, std::milli> interQuartileSum = 
-        (.25 * (timesData.size() % 4)) *    //weight of the quartile edge elements ranges from 0-.75
+        (.25 * (timesData.size() % 4)) *    //<-- quartile edge elements weight follows this expr
         (timesData[timesData.size() / 4] +  //lower quartile edge element
-        timesData[(timesData.size() - 1) - (timesData.size() / 4)]); //upper quartile edge element
+        timesData[timesData.size() - 1 - (timesData.size() / 4)]); //upper quartile edge element
 
-    //calculates arithmetic mean ahead of the others in order to find variances
+    //calculated ahead due to use in stdDev calculation 
     arithMean = std::reduce(std::next(timesData.begin()), timesData.end(), timesData.front()) / inputCount;
     
-    std::chrono::duration<long double, std::milli> sumReciprocals(0), sumLogs(0), sumSquaredDeviation(0);
+    std::chrono::duration<long double, std::milli> sumReciprocals(0), sumLogs(0), sumSquaredDeviations(0);
     for (const std::chrono::duration<long double, std::milli> time : timesData) {
-        //categorizes the time
-        categories.increment(time.count()); 
+        timeCategories.incrementAppropriateCategory(time.count()); 
 
-        //totalling of several sums to later be used in calculating respective means
-        sumReciprocals      += std::chrono::duration<long double, std::milli>(1 / time.count());
-        sumLogs             += std::chrono::duration<long double, std::milli>(logl(time.count()));
-        sumSquaredDeviation += std::chrono::duration<long double, std::milli>(powl(time.count() - arithMean.count(), 2));
+        sumReciprocals       += std::chrono::duration<long double, std::milli>(1. / time.count());
+        sumLogs              += std::chrono::duration<long double, std::milli>(logl(time.count()));
+        sumSquaredDeviations += std::chrono::duration<long double, std::milli>(powl(time.count() - arithMean.count(), 2));
 
-        //only adds to the interquartile total if within the inner quartiles
         if (time.count() > firstQuart.count() && time.count() < thirdQuart.count()) interQuartileSum += time;
     }
-    //calculates final values of means (besides arithmetic, already calculated in preparation for stdDev calculation), as well as stdDev
-    harmonMean = std::chrono::duration<long double, std::milli>(1 / (sumReciprocals / inputCount).count());
+    harmonMean = std::chrono::duration<long double, std::milli>(1. / (sumReciprocals / inputCount).count());
     geoMean =    std::chrono::duration<long double, std::milli>(expl(sumLogs.count() / inputCount));
     iqMean =     std::chrono::duration<long double, std::milli>(interQuartileSum * 2. / inputCount);
-    stdDev =     std::chrono::duration<long double, std::milli>(sqrtl(sumSquaredDeviation.count() / inputCount));
+    stdDev =     std::chrono::duration<long double, std::milli>(sqrtl(sumSquaredDeviations.count() / inputCount));
+}
+
+const size_t StatSet::getMaxValidInputCount(void) const {
+    return timesData.max_size();
 }
 
 void printDivider(std::string&& leftHeader, std::string&& rightHeader) {
-    //dictates the distance between left end of panel and the first char of the header
     static constexpr size_t indent = 3;
-    //bold underline overline bright white series of dashes with header text inserted
+    //bold, underline, overline, bright-white series of fill dashes with header text inserted left aligned to each panel, 
+    //indented according to indent
     std::println("\033[1;4;53;97m{:-<{}}{:-<{}}{:-<{}}\033[0m", "", indent, leftHeader, panelWidth, rightHeader, panelWidth - indent);
 }
--- statset.hpp
+++ statset.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cstdint>
 #include <chrono>
 #include <cmath>
@@ -16,13 +17,15 @@
 class StatSet {
 public:
     //inputCount == number of nums whose factorizations will be considered
-    StatSet(const uint64_t inputCount);
+    StatSet(const size_t inputCount);
     void printout(void) const;
     void handleNewTime(FactorCalculationInfo&& newFactorization);
     void completeFinalCalculations(void);
 
+    const size_t getMaxValidInputCount(void) const;
+
 private:
-    const uint64_t inputCount;
+    const size_t inputCount;
     //rank lists 
     FastestRankingList fastest;
     SlowestRankingList slowest;
@@ -34,7 +37,7 @@ private:
     std::chrono::duration<long double, std::milli> harmonMean, geoMean, iqMean, arithMean, stdDev;
     
     //counts of divisions of calcTimes
-    TimeCategories categories;
+    TimeCategories timeCategories;
 
     //vector of each individual calculation time
     std::vector<std::chrono::duration<long double, std::milli>> timesData;
@@ -42,8 +45,15 @@ private:
 
 void printDivider(std::string&& leftHeader = "", std::string&& rightHeader = "");
 
-//accepts a non integer index and calculates a weighted average of the adjacent elements
+//takes a target fractional percentile (0-1) and calculates a weighted average from the closest elements to that target
 template<class T>
-inline T getWeightedAverage(const double pos, const std::vector<T>& sortedList) {
-    return sortedList[(size_t)(pos)] * (1 - (pos - floor(pos))) + sortedList[(size_t)(pos + (size_t)(sortedList.size() > 1))] * (pos - floor(pos));
+inline T interpolateDataAtPercentile(const std::vector<T>& sortedList, const double fractionalPercentile) {
+    //prevents attempts to access 2 distinctly indexed elements nearest to a target when 2 such elements do not exist 
+    if (sortedList.size() < 2) 
+        return sortedList.empty() ? T(0) : sortedList.front();
+    else {
+        const double pos = (sortedList.size() - 1) * fractionalPercentile;
+        const auto pFirst = sortedList.cbegin() + static_cast<size_t>(pos);
+        return (*pFirst) + ((pos - floor(pos)) * ((*std::next(pFirst)) - (*pFirst)));
+    } 
 }
