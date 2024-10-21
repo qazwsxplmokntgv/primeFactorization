f5c6f438a04b981510ad945730c1fda0b5569806
Dylan Smith <ddsmith9254@gmail.com>
2024-10-21 13:36:55 -0700

    cleaned up extraneous comments

--- calculationinfo.cpp
+++ calculationinfo.cpp
@@ -1,13 +1,9 @@
 #include "calculationinfo.hpp"
 
 void FactorCalculationInfo::calculateAndTime(void) {
-   //start timer
     auto start = std::chrono::steady_clock::now();
-    //calculate factorization
     factorization = primes::primeFactorization(n);
-    //stop timer
     calcTime = std::chrono::duration<long double, std::milli>(std::chrono::steady_clock::now() - start);
-
 }
 
 void FactorCalculationInfo::printPostCalcInfo(void) const {
--- factorization.cpp
+++ factorization.cpp
@@ -3,26 +3,25 @@
 Factorization::Factorization() {
     //covers the vast majority of factorizations, including the slow case where the 2nd largest factor is very large
     //in worst case, requires no more than 1 reallocation (1.5x scaling assumed; still also applies to 2x)
-    //note that lengths above 10 are already very rare as well
+    //note that lengths above 11 are very rare also
     factors.reserve(11);
 }
 
 void Factorization::addNewFactor(const uint64_t base, const uint_fast8_t exp) {
-    factorCount += exp; //add to count of total factors
-    factors.emplace_back(base, exp); //append to the end of the list
+    factorCount += exp; 
+    factors.emplace_back(base, exp);
 }
 
 std::string Factorization::asString() const {
-    if (factors.empty()) //if no factors, prime factorization DNE
+    if (factors.empty()) 
         return "= DNE";
     else {
         std::string out("="); 
         for (const auto& fac : factors) {
-            //append each factor's base
             out += std::format(" {}", fac.base);
-            //if this factor's exponent is greater than 1, denote with ^exp
-            //uint_fast8_t is usually an unsigned char, hence the need for a cast
-            if (fac.exp > 1) out += std::format("^{}", (unsigned short)fac.exp);
+            //caret notation is redundant when exp <= 1
+            //uint_fast8_t is often defined as an unsigned char, hence the need for a cast
+            if (fac.exp > 1) out += std::format("^{}", static_cast<unsigned short>(fac.exp));
         }
         return out;
     }
