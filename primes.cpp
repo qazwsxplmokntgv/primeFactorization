7ad8209d74aafd11e8f7a389fd7ee6ccc2a95203
Dylan Smith <ddsmith9254@gmail.com>
2024-10-21 13:38:14 -0700

    reformatting

--- primes.cpp
+++ primes.cpp
@@ -6,36 +6,31 @@ Factorization primes::primeFactorization(uint64_t n) {
     //doubles as an flag of n's value being lowered since previous isPrime(n...) check, which results from said powers being factored out
     uint_fast8_t exp { 0 };
     //special case for multiples of nontrivial powers of 2
-    //allows evens to be cleanly skipped in the rest of the function
+    //simplifies skipping evens for the rest of this instance of the function 
     if (n > 1ull) {
         for (; !(n & 0b1); ++exp) n >>= 1;
         if (exp) foundFactors.addNewFactor(2, exp);
     }
-    //set to 1 to catch when original input (* 2^exp) is already prime 
+    //set to 1 to catch when n is already prime 
     exp = 1;
     //divides n by all odd primes until reaching the value of each of n's prime factors,
     //possibly excluding the greatest prime factor iff the square of said factor does not divide n
     uint64_t divisor { 1 };
     while (n > 1ull) {
-        //checking if the remaining n is prime
-        if (exp) { //only rechecks if n has changed since last check 
+        if (exp) { //only need to recheck if n has changed since last check 
             //n % k where 0 < k < divisor is already checked and can be skipped here, hence passing divisor as factor floor
             if (isPrime(n, divisor)) { 
                 //no n^k where integer k != 1 is prime, therefore k == 1
                 foundFactors.addNewFactor(n, 1);
                 break; //n must be the largest prime factor at this point, therefore the loop may now be exited
             } 
-            exp = 0; //resets exp following its use as a flag for unchecked modified n values
+            exp = 0; //exp no longer needs to act as a flag this iteration, resets to 0
         }
-        //skip non primes before checking for factor status
-        //divisor is always incremented at least once
+        //only relevant factors are primes
+        //note that divisor is always incremented at least once
         while (!isPrime(divisor += 2u));
-        //determine the greatest power of divisor by which n is divisible by, if any
-        while (n % divisor == 0) {
-            n /= divisor;
-            ++exp;
-        }
-        //if a non 0 power of i is a factor of n, add this to the output and move on
+
+        for (; n % divisor == 0; ++exp) n /= divisor;
         if (exp) foundFactors.addNewFactor(divisor, exp);
     }
     return foundFactors;
@@ -48,17 +43,17 @@ inline bool primes::isPrime(const uint64_t n) {
 inline bool primes::isPrime(const uint64_t n, const uint64_t potentialFactorFloor) {
     //this switch is slightly faster than any obvious bitwise approaches tested so far, which all need to first check (n < 4)
     switch (n) {
-        case 0ull: 
-        case 1ull: return false;
-        case 2ull: 
-        case 3ull: return true;
+        case 0ul: 
+        case 1ul: return false;
+        case 2ul: 
+        case 3ul: return true;
     }
 
     //handles these two cases separately to allow optimizations in the loop below
-    if (n % 2ull == 0ull || n % 3ull == 0ull) return false;
+    if (n % 2ul == 0ull || n % 3ul == 0ul) return false;
 
     //greatest integer <= the sqrt of n, truncation ok
-    const uint32_t maxLessorDivisor { (uint32_t)sqrt(n) };
+    const uint32_t maxLessorDivisor { static_cast<uint32_t>(sqrt(n)) };
 
     //iterates through all odd numbers from at least 5 through the greatest odd int <= n's sqrt inclusive
     for (uint64_t i { std::max(potentialFactorFloor, 5ul) }; i <= maxLessorDivisor; i += 2ul) {
@@ -71,61 +66,11 @@ inline bool primes::isPrime(const uint64_t n, const uint64_t potentialFactorFloo
     return true;
 }
 
-// bool primes::bailliePSW(const uint64_t n) {
-//     if (n < 0x10000) return isPrime(n);
-
-//     uint64_t d = n - 1;
-//     uint32_t s = 0;
-//     while (!(d & 0b1)) { 
-//         d >>= 1;  
-//         ++s;
-//     }
-//     bool sprpPassed = false;
-//     for (uint32_t r = 0; r < s; ++r) {
-//         if (1u >> ((1u >> r) * d) % n == 1u) {
-//             sprpPassed = true;
-//             break;
-//         }
-//     }
-//     if (!sprpPassed) return false;
-
-//     int jacobiD = 5;
-//     while (jacobiSymbol(jacobiD, n) != -1) {
-//         jacobiD = -jacobiD;
-//         jacobiD += (jacobiD > 0 ? 2 : -2);
-//     }
-
-//     const int q = (1 - jacobiD) / 4;
-//     if (lucasSequence(d, 1, q) % n == 0) {
-//         for (uint32_t r = 0; r < s; ++r) 
-//             if ((unsigned int)lucasSequence((1u >> r) * d, 1, q) == n - 1) return true;
-//     }
-//     return false;
-// }
-
-// int64_t primes::lucasSequence(const int64_t d, const int p, const int q) {
-//     std::array<int64_t, 3> values = { 2, p };
-//     for (int64_t i = 2; i < d; ++i) {
-//         values[i % 3] = p * values[(i - 1) % 3] + q * values[(i - 2) % 3];
-//     }
-//     return values[d % 3];
-// }
-
-// int primes::jacobiSymbol(int64_t a, int64_t n) {
-//     int result = 1;
-//     int64_t r;
-//     a = a % n;
-//     while (a != 0) {
-//         while (a % 2 == 0) {
-//             a /= 2;
-//             r = n % 8;
-//             if (r == 3 || r == 5) result = -result;
-//         }
-//         r = n;
-//         n = a;
-//         if (r % 4 == 3 && a % 4 == 3) result = -result;
-//         a = r % n;
-//     }
-//     if (n == 1) return result;
-//     else return 0;
-// }
+std::unordered_set<uint32_t> primes::populatePrimeSet() {
+    std::unordered_set<uint32_t> primeSet;
+    //use no more than half the total available memory
+    for (uint32_t i = 2; primeSet.size() < primeSet.max_size() / 2; ++i) 
+        if (isPrime(i)) primeSet.insert(2);
+    
+    return primeSet;
+}
--- primes.hpp
+++ primes.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <cmath>
+#include <numeric>
 #include <cstdint>
-//#include <unordered_set>
+#include <unordered_set>
 #include "factorization.hpp"
+
+//notes: isPrime(uint64_t, uint64_t) possibly use uint32_t for i? careful about overflow
 
 namespace primes {
     //returns a map of prime factors of n and their respective powers in the form key == base, val == power
@@ -11,5 +14,7 @@ namespace primes {
 
     inline bool isPrime(const uint64_t n);
     //if n is known to have no factors less than a certain number, that number can be passed in as the potentialFactorFloor
-    inline bool isPrime(const uint64_t n, const unsigned long potentialPrimeFactorFloor);
+    inline bool isPrime(const uint64_t n, const uint64_t potentialFactorFloor);
+
+    std::unordered_set<uint32_t> populatePrimeSet();
 } 
