5ff2310f168738c82380f4aea6ff08edd5693762
Dylan Smith <ddsmith9254@gmail.com>
2024-11-07 23:36:36 -0800

    small formatting change

--- calculationinfo.cpp
+++ calculationinfo.cpp
@@ -1,7 +1,7 @@
 #include "calculationinfo.hpp"
 
 void FactorCalculationInfo::calculateAndTime(void) {
-    auto start = std::chrono::steady_clock::now();
+    auto start { std::chrono::steady_clock::now() };
     factorization = primes::primeFactorization(n);
     calcTime = std::chrono::duration<long double, std::milli>(std::chrono::steady_clock::now() - start);
 }
